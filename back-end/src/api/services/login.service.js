b34259a548a941524c56b35188d1cb95cd411ef3
rodpedrassi <rodpedrassi@gmail.com>
2023-02-28 16:55:41 -0300

    feat: Criado rotas de users: findAll, getByRole, getById, update, remove

--- back-end/src/api/services/login.service.js
+++ back-end/src/api/services/login.service.js
@@ -4,14 +4,10 @@ const { User } = require('../../database/models');
 
 const login = async (email, passw) => {
     const password = crypto.createHash('md5').update(passw).digest('hex');
-    // [Será validado que não é possível fazer login com um usuário que não existe]
     const user = await User.findOne({ where: { email, password } });
     if (!user) return { type: 'NOT_FOUND', message: 'Not found' };
-    // Remove a senha do retorno da função
     const { password: _password, ...userWithoutPassword } = user.dataValues;
-    // Gerando Token
     const token = createToken(userWithoutPassword);
-    // O retorno deve ser o token
     return { token };
 };
 
