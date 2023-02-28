8446d29e21e1782a29f155d90952d35573694371
rodpedrassi <rodpedrassi@gmail.com>
2023-02-28 20:35:36 -0300

    feat: Criada as rotas de Product: Create, getByid, update, findByName, remove

--- back-end/src/api/services/products.service.js
+++ back-end/src/api/services/products.service.js
@@ -1,8 +1,40 @@
+// const { Op } = require('sequelize'); // biblioteca de operadores
 const { Product } = require('../../database/models');
 
 const getAll = async () => {
-  const user = await Product.findAll();
-  return user;
+  const product = await Product.findAll();
+  return product;
 };
 
-module.exports = { getAll };
+const create = async (product) => {
+  const productCreated = await Product.create(product);
+  return productCreated;
+};
+
+const getById = async (id) => {
+  const [result] = await Product.findAll({ where: { id } });
+  if (!result) return { type: 'ID_NOT_FOUND', message: 'Id not found' };
+  return result;
+};
+
+const update = async (id, product) => {
+  const [rowsAffected] = await Product.update(product, { where: { id } });
+  if (rowsAffected < 1) return { type: 'UPDATE_FAILED', message: 'Something went wrong' };
+  const updatedProd = await getById(Number(id));
+  return updatedProd;
+};
+
+const findByName = async (name) => {
+  const [result] = await Product.findAll({ where: { name } });
+  if (!result) return { type: 'NAME_NOT_FOUND', message: 'Name not found' };
+  return result;
+};
+
+const remove = async (id) => {
+  const [result] = await Product.findAll({ where: { id } });
+  if (!result) return { type: 'ID_NOT_FOUND', message: 'Id not found' };
+  const removed = await Product.destroy({ where: { id } });
+  return removed;
+};
+
+module.exports = { getAll, create, getById, update, remove, findByName };
