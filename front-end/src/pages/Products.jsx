33c04a7e422ce8594cddf74c222c3027ef35344b
Samuel Hyrum <samuelhymp@gmail.com>
2023-02-28 15:24:20 -0300

    feat: requisito 11, NavBar

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 
 export default function Products() {
   return (
-    <div>Products</div>
+    <div>
+      <nav>
+        <div data-testid="customer_products__element-navbar-link-products" />
+        <div data-testid="customer_products__element-navbar-link-orders" />
+        <div data-testid="customer_products__element-navbar-user-full-name" />
+        <div
+          data-testid="customer_products__element-navbar-link-logout"
+        />
+      </nav>
+    </div>
   );
 }
