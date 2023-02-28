d12cf178fcb88f276e4d849cbd4b670764f6f9fd
Leonardo Araújo <leonardoaraujofonsecaofc@gmail.com>
2023-02-28 02:41:41 -0300

    feat: add requirements 2, 3, 4 and 5
    Adiciona requisitos 2, 3, 4 e 5 o que finaliza a tela de login, além de criar a página de produtos e instalar o axios como dependência

--- back-end/src/api/services/login.service.js
+++ back-end/src/api/services/login.service.js
@@ -1,7 +1,9 @@
+const crypto = require('crypto');
 const { createToken } = require('../auth/jwtFunctions');
 const { User } = require('../../database/models');
 
-const login = async (email, password) => {
+const login = async (email, passw) => {
+    const password = crypto.createHash('md5').update(passw).digest('hex');
     // [Será validado que não é possível fazer login com um usuário que não existe]
     const user = await User.findOne({ where: { email, password } });
     if (!user) return { type: 'NOT_FOUND', message: 'Not found' };
@@ -9,7 +11,6 @@ const login = async (email, password) => {
     const { password: _password, ...userWithoutPassword } = user.dataValues;
     // Gerando Token
     const token = createToken(userWithoutPassword);
-
     // O retorno deve ser o token
     return { token };
 };
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import './App.css';
+import Products from './pages/Products';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Redirect to="/login" />
       </Route>
       <Route exact path="/login" component={ Login } />
+      <Route exact path="/customer/products" component={ Products } />
     </Switch>
 
   );
