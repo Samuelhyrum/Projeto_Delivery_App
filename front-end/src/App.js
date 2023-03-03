c149b62fc351eb593c4fadd69109e12f07bdcfec
Leonardo Araújo <leonardoaraujofonsecaofc@gmail.com>
2023-03-03 03:06:53 -0300

    feat: reqs 17, 18, 19

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,15 +10,15 @@ import CartContextProvider from './contexts/CartContext';
 function App() {
   return (
     <Switch>
+      <Route exact path="/register" component={ Register } />
+      <Route exact path="/login" component={ Login } />
       <Route exact path="/">
         <Redirect to="/login" />
       </Route>
-      <Route exact path="/login" component={ Login } />
       <CartContextProvider>
         <Route exact path="/customer/products" component={ Products } />
         <Route exact path="/customer/checkout" component={ Checkout } />
       </CartContextProvider>
-      <Route exact path="/register" component={ Register } />
     </Switch>
   );
 }
