712f88879c0cb9012a3bd04f12ad51aa522c4aa2
Leonardo Araújo <100730632+Leoarf@users.noreply.github.com>
2023-03-03 03:05:12 -0300

    fix: fix order route bug

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -12,13 +12,13 @@ function App() {
     <Switch>
       <Route exact path="/register" component={ Register } />
       <Route exact path="/login" component={ Login } />
+      <Route exact path="/">
+        <Redirect to="/login" />
+      </Route>
       <CartContextProvider>
         <Route exact path="/customer/products" component={ Products } />
         <Route exact path="/customer/checkout" component={ Checkout } />
       </CartContextProvider>
-      <Route exact path="/">
-        <Redirect to="/login" />
-      </Route>
     </Switch>
   );
 }
